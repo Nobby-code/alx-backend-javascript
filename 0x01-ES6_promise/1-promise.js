6416699d81b473b2a76c14e10bfaf3bc2ea01321
Nobby-code <norbertguda0@gmail.com>
2023-01-24 23:02:01 +0000

    boolean promise

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,6 +1,5 @@
 export default function getFullResponseFromAPI(success) {
   const myPromise = new Promise((resolve, reject) => {
-    
     if (success) {
       resolve(
         { status: 200, body: 'Success' },
